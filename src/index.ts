c3d111226734d1d0bbff86c6712547c1343e6c0d
Tanishka <tanishkadeep09@gmail.com>
2024-05-07 16:00:32 +0530

    added routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono().basePath("/api/v1");
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/user/signup", (c) => {
+  return c.text("signup route");
+});
 
-export default app
+app.post("/user/signin", (c) => {
+  return c.text("signin route");
+});
+
+app.post("/blog", (c) => {
+  return c.text("blog");
+});
+
+app.put("/blog", (c) => {
+  return c.text("blog");
+});
+
+app.get("/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("blog");
+});
+
+app.get("/blog/bulk", (c) => {
+  return c.text("all blogs");
+});
+
+export default app;
