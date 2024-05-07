e119aa688027cd2f3c2c32624937cdd7af2c555e
Tanishka <tanishkadeep09@gmail.com>
2024-05-07 16:15:09 +0530

    implemented signup route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono().basePath("/api/v1");
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>().basePath("/api/v1");
 
-app.post("/user/signup", (c) => {
-  return c.text("signup route");
+app.post("/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    return c.text("signup successful");
+  } catch (err) {
+    c.status(403);
+    return c.text("signup failed");
+  }
 });
 
 app.post("/user/signin", (c) => {
