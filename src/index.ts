39585315a03533e4fa3bf16b430d28c323d65e87
Tanishka <tanishkadeep09@gmail.com>
2024-05-08 20:10:45 +0530

    added jwt

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>().basePath("/api/v1");
 
@@ -16,14 +18,22 @@ app.post("/user/signup", async (c) => {
   const body = await c.req.json();
 
   try {
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         email: body.email,
         password: body.password,
         name: body.name,
       },
     });
-    return c.text("signup successful");
+
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.text(jwt);
   } catch (err) {
     c.status(403);
     return c.text("signup failed");
