ea7c6950689b6ce5bea63a467fc5609f422211f9
Tanishka <tanishkadeep09@gmail.com>
2024-05-10 10:18:41 +0530

    updated appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/image.jpg";
 
 export const Appbar = () => {
   return (
     <div className="bg-black py-3 px-6 text-white flex justify-center items-center">
-      <div>
+      <Link to={"/blogs"}>
         <img src={logo} alt="logo" className="w-14" />
-      </div>
-      <div className="font-custom text-4xl pl-2 font-bold">Bytes</div>
+      </Link>
+      <Link to={"/blogs"} className="font-custom text-4xl pl-2 font-bold">
+        Bytes
+      </Link>
     </div>
   );
 };
