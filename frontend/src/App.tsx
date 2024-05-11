c5a6784efb3ab0b4359219b91ecca31670ba9af9
Tanishka <tanishkadeep09@gmail.com>
2024-05-12 02:24:33 +0530

    minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/" element={<Blogs />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Create />} />
         </Routes>
